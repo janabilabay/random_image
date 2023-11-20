c3bd31b5be12cef7a870e245d06a0e71b29cd9b9
janabilabay <janabilabay@gmail.com>
2023-11-20 15:22:32 +0600

    Initial Commit

---
+++ Random Image/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Image
+//
+//  Created by Janabil on 20.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
