c7952f665ab14059fc62f369c97f55ee99f582f1
janabilabay <janabilabay@gmail.com>
2023-11-20 15:55:27 +0600

    Create UI elements

--- Random Image/Controllers/ViewController.swift
+++ Random Image/Controllers/ViewController.swift
@@ -6,13 +6,66 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    //MARK: - UI Elements
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var generateImageButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Generate", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+        
 
+    //MARK: - Setup View
+    private func setupView() {
+        [imageView, generateImageButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    //MARK: - Setup Constraints
+    private func setupConstraints() {
+        imageView.snp.makeConstraints {make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5)
+        }
+        
+        generateImageButton.snp.makeConstraints {make in
+            make.height.equalTo(50)
+            make.bottom.equalToSuperview().offset(-50)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+    }
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupConstraints()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        generateImageButton.layer.cornerRadius = 20
+        imageView.layer.cornerRadius = 30
+    }
+    
+    //MARK: - Actions
+
 }
 
