cf4a1bc11884312eef1b004952772f15e0c2e7d4
janabilabay <janabilabay@gmail.com>
2023-11-20 15:29:09 +0600

    Deleted comments, storyboard, sorted files into folders

---
+++ Random Image/Core/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  Random Image
+//
+//  Created by Janabil on 20.11.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
